f0a21ed22c560bafe79c602a4d28c18af63588f3
Artur Rymsza <artur.rymsza@gmail.com>
2017-12-07 17:25:46 +0100

    Add requested changes to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
-var span = $("span"),
-	paragraphs = $('p');
+$(function() {
+	var span = $("span"),
+		paragraphs = $('p');
 
-span.each(function(index, element) {
-	if (index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
-});
+	span.each(function(index, element) {
+		if (index % 2 === 0) {
+			$(element).css('color', 'red');
+		};
+	});
 
-paragraphs.each(function(index, element) {
-	var button = '<button class="btn" data-tmp=' + index + '">Kliknij mnie</button>'
-	$(element).append(button);
-});
+	paragraphs.each(function(index, element) {
+		var button = '<button class="btn" data-tmp=' + index + '">Kliknij mnie</button>'
+		$(element).append(button);
+	});
 
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+	$("button").on('click', function() {
+		alert($(this).attr("data-tmp"));
+	});
 });
